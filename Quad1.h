ec79fe7f891c28bd0d403dd0224c5e11160f1d5f
Leafl <emma.theberge@gmail.com>
2017-11-04 15:59:03 -0400

    Update Quad1.h

--- Quad1.h
+++ Quad1.h
@@ -3,7 +3,7 @@
  * Project 4
  * some code may be copied and altered from
  * Triangle example
- * Code taken and altered from Unit6 Texture
+ * Code taken and altered from Unit6 Texture and unit 7 lighting
  * Example
  *
  * Emma Theberge
@@ -15,44 +15,59 @@
  *** GRAPHICS INCLUDES ***
  ************************/
 #include <GL/glew.h>
-#include "DrawableObject.h"
-#include "Texture.hpp"
-#include "Shader.hpp"
-//#include <glm/gtc/matrix_transform.hpp>
 #include <glm/glm.hpp>
 #include <iostream>
 #include <iomanip>
 #include <stdio.h>
 
+#include "Triangle.hpp"
+#include "Texture.hpp"
+#include "Vertex.hpp"
+#include "Shader.hpp"
+//#include <glm/gtc/matrix_transform.hpp>
+
 using glm::vec3;
 using glm::vec4;
 
-class Quad1//: public DrawableObject
+class Quad1
 {
 public:
 	Quad1();
-	Quad1(vec3 _a, vec3 _b, vec3 _c, vec3 _d);
+	Quad1(Vertex* _a, Vertex* _b, Vertex* _c, Vertex* _d);
+	Quad1(const Quad1& orig);
+	
 	void setFillColor(vec3 fC);
 	void setBorderColor(vec3 bC);
+	
+	// Calculate the normal
+	void calculateNormal();
 
 	// The draw functions
 	void draw(Shader*, Texture* = NULL);
 
-	void setIsSelected(bool select);
-	bool getIsSelected();
-
+	virtual ~Quad1();
+	
+	// Getters and Setters
+    vec3 getNormal1() { return normal1; }
+	vec3 getNormal2() { return normal2; }
+	
+    Vertex* getVa() { return va; }
+    Vertex* getVb() { return vb; }
+    Vertex* getVc() { return vc; }
+	Vertex* getVd() { return vd; }
 private:
 	// Colors
 	vec3 fillColor;
 	vec3 borderColor;
 
-	bool isSelected = true;
-
 	//hold all four verticies of the quad
-	vec3 a;
-	vec3 b;
-	vec3 c;
-	vec3 d;
+	Vertex* a;
+	Vertex* b;
+	Vertex* c;
+	Vertex* d;
+	
+	vec3 normal1;
+	vec3 normal2;
 
 	GLuint VertexArrayObject;
 	GLuint VertexBufferObject;
